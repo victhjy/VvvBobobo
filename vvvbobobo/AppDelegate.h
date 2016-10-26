b230d47671b594fc5b8ee8105be6d580d6e03a70
git <873965585@qq.com>
2016-10-26 10:19:27 +0800

    Initial Commit

---
+++ vvvbobobo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vvvbobobo
+//
+//  Created by huangjinyang on 16/10/26.
+//  Copyright © 2016年 huangjinyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
